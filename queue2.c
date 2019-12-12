094a32bceecd252dec363282d88c28873423e11e
Syouki Tsuyama <s-tsuyama@economic-index.co.jp>
2019-12-12 19:05:10 +0900

    fix

--- queue2.c
+++ queue2.c
@@ -29,8 +29,7 @@ queue_t *create_queue_from_list(list_t *data) {
   return create_queue(head, tail);
 }
 
-
-list_t *create_list(list_t prev, int val, list_t *next) {
+list_t *create_list(int val, list_t *next) {
   list_t *list = malloc(sizeof(list_t));
   list->val  = val;
   list->next = next;
@@ -40,7 +39,9 @@ list_t *create_list(list_t prev, int val, list_t *next) {
 // 副作用がないほう
 queue_t *enqueue(queue_t *queue, int x) {
   list_t *head = queue->head;
-  list_t *tail = create_list(queue->tail, x, NULL);
+  
+  list_t *new_tail = create_list(x, NULL);
+  queue->tail  = new_tail;
   return create_queue(head, tail);
 }
 
@@ -49,10 +50,20 @@ void enqueue(queue_t *queue, int x) {
   queue->tail = create_list(tail, x, NULL);
 }
 
+// 副作用があるほう
 int dequeue(queue_t *queue) {
   int val = queue->head->val;
   queue->head = queue->head->next;
   return val;
+}
+
+// 副作用がないほう
+int get_queue(queue_t *queue) {
+  return queue->head->val;
+}
+
+int pop_queue(queue_t *queue) {
+  return create_queue(queue->head->next, queue->tail);
 }
 
 void print_queue(queue_t *queue) {
